16c0f211bd46156bfcbe3f60f9875171ae59448c
renka <r.oomori.sys22@morijyobi.ac.jp>
2024-01-29 11:29:02 +0900

    スライドショー仮作成 メインコンテンツの作成 #3

--- index.php
+++ index.php
@@ -10,11 +10,28 @@
     <header>
     
     </header>
-
+    <section>
+        <!-- https://kodocode.net/design-css-carousel/ -->
+        <div id="carousel">
+            <figure id="spinner">
+                <img src="img/slide_img4.jpg" alt>
+                <img src="img/slide_img1.jpg" alt>
+                <img src="img/slide_img2.jpg" alt>
+                <img src="img/slide_img3.jpg" alt>
+                <img src="img/slide_img4.jpg" alt>
+                <img src="img/slide_img1.jpg" alt>
+                <img src="img/slide_img2.jpg" alt>
+                <img src="img/slide_img3.jpg" alt>
+            </figure>
+        </div>
+        <span style="float:left" class="ss-icon" onclick="galleryspin('-')">&lt;</span>
+        <span style="float:right" class="ss-icon" onclick="galleryspin('')">&gt;</span>
+    </section>
 
     
     <footer>
 
     </footer>
+    <script src="js/script.js"></script>
 </body>
 </html>
