2145991d97232f45f166566fcab339bcc17fd53e
renka <r.oomori.sys22@morijyobi.ac.jp>
2024-01-29 11:47:06 +0900

    テキスト配置 メインコンテンツの作成 #3

--- index.php
+++ index.php
@@ -27,6 +27,13 @@
         <span style="float:left" class="ss-icon" onclick="galleryspin('-')">&lt;</span>
         <span style="float:right" class="ss-icon" onclick="galleryspin('')">&gt;</span>
     </section>
+    <section>
+        <p>キャッチコピー</p>
+        <p>いつでもどこでもだれにでも自然な笑顔</p>
+        <p>そこにホスピタリティマインドと<br>ビジネスマナー研修で好印象</p>
+        <p>自分の容姿に自信がない<br>人付き合いに自信がない<br>かつての私もそうでした</p>
+        <p>最高の好印象笑顔で自信を持ってチャンスをつかむ</p>
+    </section>
 
     
     <footer>
