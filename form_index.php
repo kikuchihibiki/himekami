728928f3d0905477c62ecc03695b99eb856d7b31
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2024-02-05 11:43:39 +0900

    [add]formのタブ切り替えの実装

---
+++ form_index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/form_style.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <header>
+      <h1><span>工藤晴香</span> h.k smile office</h1>
+      <div>
+        <a href="">セミナー依頼</a>
+        <a href="">お問い合わせ</a>
+      </div>
+      <ul>
+        <li><a href="">セミナーについて</a></li>
+        <li><a href="">セミナー開始までの流れ</a></li>
+        <li><a href="">セミナーの感想</a></li>
+        <li><a href="">スマイルトレーナーとは</a></li>
+        <li><a href="">講師紹介</a></li>
+      </ul>
+    </header>
+    <section class="inquiry" id="inquiry">
+      <div class="tabs">
+        <input id="employee_training" type="radio" name="tab_item" checked />
+        <label class="tab_item" for="employee_training">お問い合わせ</label>
+        <input id="smile_training" type="radio" name="tab_item" />
+        <label class="tab_item" for="smile_training">依頼</label>
+
+        <div class="tab_content" id="training_content">
+          <div class="tab_content_description">
+            <p class="c-txtsp"></p>
+            <label>お問い合わせ</label>
+            <form method="post" action="#">
+              <p>名前 <input type="text" /></p>
+              <p>企業名 <input type="text" /></p>
+              <p>連絡先電話番号 <input type="text" /></p>
+              <p>連絡先E-mailアドレス <input type="text" /></p>
+              <textarea></textarea>
+              <button type="submit">送信</button>
+            </form>
+          </div>
+        </div>
+        <div class="tab_content" id="smile_content">
+          <div class="tab_content_description">
+            <p class="c-txtsp"></p>
+            <label>依頼</label>
+            <form method="post" action="#">
+              <p>名前 <input type="text" /></p>
+              <p>連絡先電話番号 <input type="text" /></p>
+              <p>連絡先E-mailアドレス <input type="text" /></p>
+              <button type="submit">送信</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <p>工藤晴香 h.k smile office</p>
+      <p>hksmileoffice@gmail.com</p>
+      <p>000-0000-0000</p>
+      <a href="">インスタ</a>
+      <a href="">Facebook</a>
+    </footer>
+  </body>
+</html>
